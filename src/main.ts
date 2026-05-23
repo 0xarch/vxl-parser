21d027cff21fb8772ecf11f55462c87871e0946a
0xarch <alanqa-ops@outlook.com>
2026-05-23 17:40:02 +0800

    plug-ize

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,57 @@
 import { readFileSync, writeFileSync } from "node:fs";
 import VXLPlugin from "./plugin/vxl/main";
 import VOXPlugin from "./plugin/vox/main";
-import { voxPalToStandardPal } from "./tool/vox_pal";
+import { standardPalToVoxPal, voxPalToStandardPal } from "./tool/vox_pal";
 
-const file_name = process.argv[2] ?? 'test.vox';
+const SupportedTypes = ['vxl', 'vox'];
+type __types = 'vxl' | 'vox'
 
-const output_file_name = process.argv[3] ?? `parsed.vxl`;
-// const output_file_name = process.argv[3] ?? `${file_name}_parsed.vxl`;
+// expected like "vxl-vox" , ...
+const convert_type: [__types, __types] = (_ => {
+    let [from, to] = process.argv[2].split('-');
+    if(!SupportedTypes.includes(from)) {
+        throw new Error(`Unsupported FROM type: ${from}`);
+    }
+    if(!SupportedTypes.includes(to)) {
+        throw new Error(`Unsupported TO type: ${to}`);
+    }
+    return [from, to] as [__types, __types];
+})();
+
+const file_name = process.argv[3] ?? `input.${convert_type[0]}`;
+
+const output_file_name = process.argv[4] ?? `output.${convert_type[1]}`;
 
 console.log(`Parsing: ${file_name} -> ${output_file_name}`);
 
 const raw_buffers = readFileSync(file_name).buffer;
 
-// const vxl = VXLPlugin.parse(raw_buffers);
+let standard_voxel = (() => {
+    if (convert_type[0] === 'vox') {
+        let vox = VOXPlugin.parse(raw_buffers);
+        return VOXPlugin.standarize(vox);
+    } else {
+        let vxl = VXLPlugin.parse(raw_buffers);
+        return VXLPlugin.standarize(vxl);
+    }
+})();
 
-// console.log(vxl.limb_tailer[0]);
+if(convert_type[0] === 'vox') {
+    standard_voxel = voxPalToStandardPal(standard_voxel);
+}
 
-// const parsed_buffers = VXLPlugin.rebuild(vxl);
+if(convert_type[1] === 'vox') {
+    standard_voxel = standardPalToVoxPal(standard_voxel);
+}
 
-const vox = VOXPlugin.parse(raw_buffers);
+const output = (()=>{
+    if (convert_type[1] === 'vox') {
+        let vox = VOXPlugin.toLocalType(standard_voxel);
+        return VOXPlugin.compile(vox);
+    } else {
+        let vxl = VXLPlugin.toLocalType(standard_voxel);
+        return VXLPlugin.compile(vxl);
+    }
+})();
 
-const voxel = VOXPlugin.standarize(vox);
-
-const ra_voxel = voxPalToStandardPal(voxel);
-
-const vxl = VXLPlugin.toLocalType(ra_voxel);
-
-const parsed_buffers = VXLPlugin.compile(vxl);
-// const parsed_buffers = VOXPlugin.compile(VOXPlugin.toLocalType(ra_voxel));
-
-writeFileSync(output_file_name, Buffer.from(parsed_buffers));
+writeFileSync(output_file_name, Buffer.from(output));
